bdca55e5459dbc42bed7cf814fba0e5604db65f7
Wes <wal2.designer@gmail.com>
2024-08-03 16:47:29 +0100

    User insert

--- src/main/java/com/iobit/comercio_vizinho_ddd_2/resources/UserResource.java
+++ src/main/java/com/iobit/comercio_vizinho_ddd_2/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.iobit.comercio_vizinho_ddd_2.services.UserService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,6 +31,19 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = service.findById(id);
         return ResponseEntity.ok()
+                             .body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI
+                uri =
+                ServletUriComponentsBuilder.fromCurrentRequest()
+                                           .path("/{id}")
+                                           .buildAndExpand(obj.getId())
+                                           .toUri();
+        return ResponseEntity.created(uri)
                              .body(obj);
     }
 }
