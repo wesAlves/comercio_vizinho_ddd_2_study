d5cfd62316548487419b0e3e621fb4ece38d3170
Wes <wal2.designer@gmail.com>
2024-08-03 16:16:32 +0100

    SubTotal and Total

--- src/main/java/com/iobit/comercio_vizinho_ddd_2/entities/Payment.java
+++ src/main/java/com/iobit/comercio_vizinho_ddd_2/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.iobit.comercio_vizinho_ddd_2.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -15,6 +16,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
