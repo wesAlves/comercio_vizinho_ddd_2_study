cf27f74f3182d6f764ed917bb0b0e0c21377b7eb
Wes <wal2.designer@gmail.com>
2024-08-03 16:52:27 +0100

    User delete

--- src/main/java/com/iobit/comercio_vizinho_ddd_2/resources/UserResource.java
+++ src/main/java/com/iobit/comercio_vizinho_ddd_2/resources/UserResource.java
@@ -46,4 +46,12 @@ public class UserResource {
         return ResponseEntity.created(uri)
                              .body(obj);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent()
+                             .build();
+    }
 }
